8da681764e9577994581a5216e002e6e48f648ae
Vivek Vijay Sinha <VivekVijay.Sinha@kas-services.com>
2021-04-06 10:19:21 +0530

    added more steps

--- flipr/sqitch.ps1
+++ flipr/sqitch.ps1
@@ -1,3 +1,2 @@
 # Run the container with the current and home directories mounted.
-echo $args
-docker run -it --rm --network host --mount "type=bind,src=$pwd,dst=/repo" --mount "type=bind,src=$HOME,dst=/home" sqitch/sqitch $args
+docker run -it --rm --network host --mount "type=bind,src=$pwd,dst=/repo" --mount "type=bind,src=$pwd,dst=/home" sqitch/sqitch $args
